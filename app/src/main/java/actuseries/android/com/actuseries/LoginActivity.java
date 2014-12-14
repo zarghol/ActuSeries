a6bcbea39a01b83a7e2f190ad1fb9437f7c876fe
lightningff13 <charlybrugere@gmail.com>
2014-12-14 11:58:35 +0100

    Ajout des vues Connexion et Ajout nouveau compte
    A voir: amélioration du design

--- app/src/main/java/actuseries/android/com/actuseries/LoginActivity.java
+++ app/src/main/java/actuseries/android/com/actuseries/LoginActivity.java
@@ -1,17 +1,17 @@
 package actuseries.android.com.actuseries;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MyActivity extends Activity {
+public class LoginActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.signup_activity);
     }
 
 
